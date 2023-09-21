0cc63619d0350345ed5c70f9461dada2dcf8e6e1
omar <omar.rebai@enis.tn>
2023-09-21 09:07:24 +0100

    first commit

--- member/src/main/java/tn/enis/member/entities/Etudiant.java
+++ member/src/main/java/tn/enis/member/entities/Etudiant.java
@@ -1,11 +1,15 @@
 package tn.enis.member.entities;
 
+import org.springframework.lang.NonNull;
+
 import javax.persistence.Entity;
 import java.util.Date;
 
 @Entity
 public class Etudiant extends Member {
+    @NonNull
     private Date dateInscription;
+    @NonNull
     private String Diplome;
 
 }
