9d353ba46a699f1aaca8488cdc6ca8790c8bac8b
omar <omar.rebai@enis.tn>
2023-10-21 10:18:42 +0100

    Optimizing imports

--- member/src/main/java/tn/enis/member/proxies/EvenementProxyService.java
+++ member/src/main/java/tn/enis/member/proxies/EvenementProxyService.java
@@ -1,13 +1,9 @@
 package tn.enis.member.proxies;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import tn.enis.member.beans.EvenementBean;
-import tn.enis.member.beans.OutilBean;
-
-import java.util.Optional;
 
 @FeignClient(name = "EVENEMENT-SERVICE")
 public interface EvenementProxyService {
--- member/src/main/java/tn/enis/member/proxies/OutilProxyService.java
+++ member/src/main/java/tn/enis/member/proxies/OutilProxyService.java
@@ -1,12 +1,9 @@
 package tn.enis.member.proxies;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import tn.enis.member.beans.OutilBean;
-
-import java.util.Optional;
 
 @FeignClient(name = "OUTIL-SERVICE")
 public interface OutilProxyService {
